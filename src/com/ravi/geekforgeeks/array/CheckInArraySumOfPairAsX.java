40a42cce0dab69a0a175e1ef8094f8dd89489dbc
ravi <ravisinghsw@gmail.com>
2017-07-06 09:50:39 +0530

    question complete

--- src/com/ravi/geekforgeeks/array/CheckInArraySumOfPairAsX.java
+++ src/com/ravi/geekforgeeks/array/CheckInArraySumOfPairAsX.java
@@ -1,13 +1,8 @@
-package com.ravi.geekforgeeks;
+package com.ravi.geekforgeeks.array;
 
 /**
  * Created by ravi on 7/3/2017.
  */
-
-import java.util.Arrays;
-import java.util.stream.Stream;
-
-import static java.util.stream.Stream.of;
 
 /**
  * Example:
@@ -25,10 +20,28 @@ import static java.util.stream.Stream.of;
 
  Note: If there are more than one pair having the given sum then this algorithm reports only one. Can be easily extended for this though.
  http://www.fluentland.com/how-to-give-opinion-in-english/
+
+ http://www.geeksforgeeks.org/write-a-c-program-that-given-a-set-a-of-n-numbers-and-another-number-x-determines-whether-or-not-there-exist-two-elements-in-s-whose-sum-is-exactly-x/
  */
 public class CheckInArraySumOfPairAsX {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args){
+        int [] arr={1, 4, 45, 6, 10, -8};
+        int value = 16;
+
+        boolean[] b = new boolean[100000];
+        for(int i=0;i<arr.length ; i++){
+
+            int temp = value - arr[i];
+            if(temp >=0 && b[temp]){
+                System.out.println(" value found  " + arr[i] + " "+ temp );
+                break;
+            }
+            b[arr[i]] = true;
+        }
+    }
+
+    public static void main1(String[] args) throws Exception{
 
         int [] arr={1, 4, 45, 6, 10, -8};
 
@@ -43,6 +56,20 @@ public class CheckInArraySumOfPairAsX {
         for (int  x : arr) {
             System.out.print(x + " ");
         }
+        System.out.print( "\n\n ");
+        boolean isFound = false;
+        for(int i = 1 ; i <=arr.length ; i++){
+
+            if(value == arr[i-1] + arr[i]) {
+                System.out.println(arr[i-1] +"---"+ arr[i]);
+                isFound = true;
+                break;
+            }
+        }
+
+        if(!isFound){
+            System.out.println(" Value not found ");
+        }
     }
 
 
@@ -56,10 +83,7 @@ public class CheckInArraySumOfPairAsX {
                      arr[i] =  arr[k];
                     arr[k] = temp;
                     i--;
-
                 }
-
-
                 k--;
             }
 
